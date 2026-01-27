c20c60678d71353adc07b5b227d1fe8bba7cbd05
Qfactor <qfactor2013@gmail.com>
2026-01-27 13:45:58 +0900

    Spring 3.5.10 initialize

---
+++ src/main/java/com/viewty/viewtyback/ViewtyBackApplication.java
@@ -0,0 +1,13 @@
+package com.viewty.viewtyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewtyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewtyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewty/viewtyback/ViewtyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewty.viewtyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewtyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
